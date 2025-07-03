f4b600c3991fcb72c8a1b92687332499c8e9f069
Bernd Weigel <b.weigel@xceptance.com>
2025-07-03 06:49:10 +0200

    [#369] added additional unit tests for pop up blocking

--- src/test/java/com/xceptance/neodymium/junit4/testclasses/popupblocker/PopupBlockerTestclass.java
+++ src/test/java/com/xceptance/neodymium/junit4/testclasses/popupblocker/PopupBlockerTestclass.java
@@ -1,7 +1,9 @@
 package com.xceptance.neodymium.junit4.testclasses.popupblocker;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +23,7 @@ public class PopupBlockerTestclass extends NeodymiumTest
         Selenide.open("https://www.xceptance.com/");
         String popup = "var e = document.createElement('div');"
                        + "e.innerHTML = 'testThing';"
-                       + "e.setAttribute('id','myWindow');"
+                       + "e.setAttribute('id','myPopUp1');"
                        + "e.setAttribute('onclick','this.remove()');"
                        + "document.body.appendChild(e);";
         Selenide.executeJavaScript(popup, "");
@@ -49,7 +51,7 @@ public class PopupBlockerTestclass extends NeodymiumTest
         Selenide.open("https://www.xceptance.com/");
         String popup = "var e = document.createElement('svg');"
                        + "e.innerHTML = 'testThing';"
-                       + "e.setAttribute('id','myWindow');"
+                       + "e.setAttribute('id','myPopUp1');"
                        + "e.setAttribute('onclick','this.remove()');"
                        + "document.body.appendChild(e);";
         Selenide.executeJavaScript(popup, "");
@@ -72,10 +74,65 @@ public class PopupBlockerTestclass extends NeodymiumTest
     }
 
     @Test
+    public void testMultiplePopUpsBlocked()
+    {
+        Selenide.open("https://www.xceptance.com/");
+        String popup1 = "var e = document.createElement(\"div\");\r\n"
+                        + "e.innerHTML = \"testThing\";\r\n"
+                        + "e.setAttribute('id','myPopUp1');\r\n"
+                        + "e.setAttribute('onclick','this.remove()');\r\n"
+                        + "document.body.appendChild(e);";
+        String popup2 = "var e = document.createElement(\"div\");\r\n"
+                        + "e.innerHTML = \"testThing\";\r\n"
+                        + "e.setAttribute('id','myPopUp2');\r\n"
+                        + "e.setAttribute('onclick','this.remove()');\r\n"
+                        + "document.body.appendChild(e);";
+        String popup3 = "var e = document.createElement(\"div\");\r\n"
+                        + "e.innerHTML = \"testThing\";\r\n"
+                        + "e.setAttribute('id','myPopUp3');\r\n"
+                        + "e.setAttribute('onclick','this.remove()');\r\n"
+                        + "document.body.appendChild(e);";
+        Selenide.executeJavaScript(popup1, "");
+        Selenide.executeJavaScript(popup3, "");
+        Selenide.executeJavaScript(popup2, "");
+        Selenide.sleep(1500);
+        $("#myPopUp1").shouldNotBe(visible);
+        $("#myPopUp2").shouldNotBe(visible);
+        $("#myPopUp3").shouldNotBe(visible);
+    }
+
+    @Test
+    public void testPopUpsBlockedAfterAdditionalPageLoad()
+    {
+        Selenide.open("https://www.xceptance.com/");
+        String popup1 = "var e = document.createElement(\"div\");\r\n"
+                        + "e.innerHTML = \"testThing\";\r\n"
+                        + "e.setAttribute('id','myPopUp1');\r\n"
+                        + "e.setAttribute('onclick','this.remove()');\r\n"
+                        + "document.body.appendChild(e);";
+        Selenide.executeJavaScript(popup1, "");
+        Selenide.sleep(1500);
+        $("#myPopUp1").shouldNotBe(visible);
+        
+        // Next page by load
+        Selenide.open("https://blog.xceptance.com/");
+        Selenide.executeJavaScript(popup1, "");
+        Selenide.sleep(1500);
+        $("#myPopUp1").shouldNotBe(visible);
+        
+        // next page by click
+        $$(".blogroll > li > a").findBy(exactText("XLT")).click();
+        Selenide.executeJavaScript(popup1, "");
+        Selenide.sleep(1500);
+        $("#myPopUp1").shouldNotBe(visible);
+
+    }
+
+    @Test
     public void testWithNoPopUp()
     {
         Selenide.open("https://www.xceptance.com/");
         Selenide.sleep(1500);
-        $("#myWindow").shouldNotBe(visible);
+        $("#myPopUp1").shouldNotBe(visible);
     }
 }
