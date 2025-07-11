d3a36d7dc3d5e744c6395010bf6da371b5070fa4
RobertAvemarg <jan.robert.avemarg@gmail.com>
2025-07-11 11:57:09 +0200

    add screenshot for open() and log messages for missing driver

--- src/main/java/com/xceptance/neodymium/common/AllureTestStepListener.java
+++ src/main/java/com/xceptance/neodymium/common/AllureTestStepListener.java
@@ -4,6 +4,8 @@ import com.xceptance.neodymium.util.AllureAddons;
 import com.xceptance.neodymium.util.Neodymium;
 import io.qameta.allure.listener.StepLifecycleListener;
 import io.qameta.allure.model.StepResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -12,6 +14,10 @@ import static io.qameta.allure.model.Status.PASSED;
 
 public class AllureTestStepListener implements StepLifecycleListener
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AllureTestStepListener.class);
+
+    private static boolean driverNullWarningLogged = false;
+
     @Override
     public void beforeStepStop(StepResult result)
     {
@@ -23,8 +29,16 @@ public class AllureTestStepListener implements StepLifecycleListener
             return;
         }
 
+        // if the driver is not set, we do not need to do anything
         if (Neodymium.getDriver() == null)
         {
+            if (!driverNullWarningLogged)
+            {
+                LOGGER.warn(
+                    "Driver is not set in Neodymium and Selenide driver is used. Skipping screenshot capture for each test step. To enable screenshots per" +
+                        " step, add the @Browser annotation to your test class or method.");
+                driverNullWarningLogged = true;
+            }
             return;
         }
 
@@ -43,7 +57,12 @@ public class AllureTestStepListener implements StepLifecycleListener
         }
 
         // skip screenshots for steps with names starting with $(, $$(, $x(, or $$x( or for URL changed steps
-        if (name.startsWith(URL_CHANGED_STEP_MESSAGE) || name.startsWith("$(") || name.startsWith("$$(") || name.startsWith("$x(") || name.startsWith("$$x("))
+        // but don't skip steps that open a URL, to ensure that we capture the initial page load
+        if (name.startsWith(URL_CHANGED_STEP_MESSAGE)
+            || (name.startsWith("$(") && !name.startsWith("$(\"open\") "))
+            || name.startsWith("$$(")
+            || name.startsWith("$x(")
+            || name.startsWith("$$x("))
         {
             return;
         }
