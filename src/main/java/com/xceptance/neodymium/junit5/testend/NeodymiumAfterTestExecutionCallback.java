dcf71807e455b1615578b068cecb8520162c2605
Bernd Weigel <b.weigel@xceptance.com>
2025-07-24 13:34:08 +0200

    [#393]added test stage for correct screenshot handling

--- src/main/java/com/xceptance/neodymium/junit5/testend/NeodymiumAfterTestExecutionCallback.java
+++ src/main/java/com/xceptance/neodymium/junit5/testend/NeodymiumAfterTestExecutionCallback.java
@@ -1,15 +1,18 @@
 package com.xceptance.neodymium.junit5.testend;
 
-import com.xceptance.neodymium.common.ScreenshotWriter;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+
+import com.xceptance.neodymium.common.ScreenshotWriter;
+import com.xceptance.neodymium.common.TestStage;
 
 public class NeodymiumAfterTestExecutionCallback implements AfterTestExecutionCallback
 {
     @Override
     public void afterTestExecution(ExtensionContext context) throws Exception
     {
+        System.out.println("\tNeodymiumAfterTestExecutionCallback");
         ScreenshotWriter.doScreenshot(context.getRequiredTestMethod().getName(), context.getRequiredTestClass().getName(), context.getExecutionException(),
-                                      context.getRequiredTestMethod().getAnnotations());
+                                      context.getRequiredTestMethod().getAnnotations(), TestStage.TEST);
     }
 }
