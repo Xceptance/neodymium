819904f19cec396fc1e09ca8027f849aae72420b
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2025-07-31 13:20:30 +0200

    [#53] imrpove junit5 retry implementation

--- src/main/java/com/xceptance/neodymium/junit5/AbortRetryOnSuccess.java
+++ src/main/java/com/xceptance/neodymium/junit5/AbortRetryOnSuccess.java
@@ -1,15 +1,26 @@
 package com.xceptance.neodymium.junit5;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.InvocationInterceptor;
+import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.junit.jupiter.api.extension.InvocationInterceptor.Invocation;
 
 import com.xceptance.neodymium.common.retry.RetryMethodData;
+import com.xceptance.neodymium.common.retry.TestFailedAndShouldBeRetired;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.model.Stage;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StatusDetails;
 
 public class AbortRetryOnSuccess
-    implements BeforeEachCallback, TestExecutionExceptionHandler, TestWatcher
+    implements BeforeEachCallback, TestExecutionExceptionHandler, InvocationInterceptor, TestWatcher
 {
     private RetryMethodData retryMethodData;
 
@@ -31,14 +42,66 @@ public class AbortRetryOnSuccess
     @Override
     public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable
     {
-        retryMethodData.handleTestExecutionException(throwable);
-        throw throwable;
+        Throwable toThrow = retryMethodData.handleTestExecutionException(throwable);
+        // if (toThrow instanceof TestFailedAndShouldBeRetired)
+        // {
+        // Allure.getLifecycle()
+        // .updateTestCase(r -> r.setStatus(Status.FAILED).setStatusDetails(new
+        // StatusDetails().setMuted(false).setMessage(throwable.getMessage())));
+        // }
+        throw toThrow;
+    }
+
+    @Override
+    public void testAborted(ExtensionContext context, Throwable cause)
+    {
+        if (cause instanceof TestFailedAndShouldBeRetired)
+        {
+            Allure.getLifecycle().updateTestCase(r -> r.setStage(Stage.FINISHED));
+            Allure.getLifecycle().updateTestCase(r -> r.setStatus(Status.FAILED));
+            Allure.getLifecycle().updateTestCase(r -> r.setStatusDetails(new StatusDetails().setMuted(false).setMessage(cause.getCause().getMessage())));
+        }
+    }
+
+    @Override
+    public void interceptBeforeEachMethod(Invocation<Void> invocation,
+                                          ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext)
+        throws Throwable
+    {
+        try
+        {
+            invocation.proceed();
+        }
+        catch (Throwable e)
+        {
+            throw retryMethodData.handleTestExecutionException(e);
+        }
+    }
+
+    @Override
+    public void interceptAfterEachMethod(Invocation<Void> invocation,
+                                         ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext)
+        throws Throwable
+    {
+        try
+        {
+            invocation.proceed();
+        }
+        catch (Throwable e)
+        {
+            throw retryMethodData.handleTestExecutionException(e);
+        }
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause)
     {
+        Throwable t = retryMethodData.getThrowable(cause);
         retryMethodData.testFailed(cause);
+        if (t instanceof TestFailedAndShouldBeRetired)
+        {
+            Assumptions.abort(t.getMessage());
+        }
     }
 
     @Override
