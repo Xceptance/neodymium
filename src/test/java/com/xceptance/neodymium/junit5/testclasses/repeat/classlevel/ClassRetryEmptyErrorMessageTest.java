7a59fb0f2237d980edb9b255beb190217a984db8
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2025-08-11 09:03:49 +0200

    [#53] stage ClassRetryEmptyErrorMessageTest classes

---
+++ src/test/java/com/xceptance/neodymium/junit5/testclasses/repeat/classlevel/ClassRetryEmptyErrorMessageTest.java
@@ -0,0 +1,22 @@
+package com.xceptance.neodymium.junit5.testclasses.repeat.classlevel;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Assert;
+
+import com.xceptance.neodymium.common.browser.Browser;
+import com.xceptance.neodymium.common.retry.Retry;
+import com.xceptance.neodymium.junit5.NeodymiumTest;
+
+@Browser("Chrome_headless")
+@Retry
+public class ClassRetryEmptyErrorMessageTest
+{
+    static AtomicInteger i = new AtomicInteger();
+
+    @NeodymiumTest
+    public void test()
+    {
+        Assert.fail();
+    }
+}
