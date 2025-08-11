f0ec1b2b69b561c36dc09ea40b17906d164bde3d
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2025-08-11 12:42:11 +0200

    [#53] improve error message for test that will be retried

--- src/main/java/com/xceptance/neodymium/common/retry/TestFailedAndShouldBeRetired.java
+++ src/main/java/com/xceptance/neodymium/common/retry/TestFailedAndShouldBeRetired.java
@@ -6,7 +6,7 @@ public class TestFailedAndShouldBeRetired extends TestAbortedException
 {
     public TestFailedAndShouldBeRetired(int nextRetryIndex, Throwable originalError)
     {
-        super("Abort test and retry for the " + nextRetryIndex + " time", originalError);
+        super("Test failed, starting retry: " + nextRetryIndex, originalError);
     }
 
     private static final long serialVersionUID = 1L;
