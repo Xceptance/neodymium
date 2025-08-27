fc2975d9284739682d5c2b3dd99ee2de297d488c
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2025-08-27 09:36:35 +0200

    [#385] add unit tests

---
+++ src/test/java/com/xceptance/neodymium/junit5/tests/DriverCustomCapabilitesTest.java
@@ -0,0 +1,58 @@
+package com.xceptance.neodymium.junit5.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+
+import com.xceptance.neodymium.common.browser.configuration.MultibrowserConfiguration;
+import com.xceptance.neodymium.junit4.NeodymiumRunner;
+import com.xceptance.neodymium.junit4.testclasses.webDriver.DriverCustomCapabilitesTestClass;
+import com.xceptance.neodymium.junit5.testclasses.webDriver.DriverArgumentsConfigTest;
+import com.xceptance.neodymium.junit5.tests.utils.NeodymiumTestExecutionSummary;
+
+public class DriverCustomCapabilitesTest extends AbstractNeodymiumTest
+{
+
+    @BeforeClass
+    public static void createSettings() throws IOException
+    {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("browserprofile.FF_headless.name", "FF headless");
+        properties.put("browserprofile.FF_headless.headless", "true");
+        properties.put("browserprofile.FF_headless.browserResolution", "1024x768");
+        properties.put("browserprofile.FF_headless.browser", "firefox");
+
+        properties.put("browserprofile.FF_with_capability.name", "FF with capability");
+        properties.put("browserprofile.FF_with_capability.headless", "true");
+        properties.put("browserprofile.FF_with_capability.browserResolution", "1024x768");
+        properties.put("browserprofile.FF_with_capability.browser", "firefox");
+        properties.put("browserprofile.FF_with_capability.capability.unhandledPromptBehavior", "accept");
+
+        properties.put("browserprofile.Chrome_with_capability.name", "Chrome with capability");
+        properties.put("browserprofile.Chrome_with_capability.headless", "true");
+        properties.put("browserprofile.Chrome_with_capability.browserResolution", "1024x768");
+        properties.put("browserprofile.Chrome_with_capability.browser", "chrome");
+        properties.put("browserprofile.Chrome_with_capability.capability.unhandledPromptBehavior", "accept");
+        File tempConfigFile = File.createTempFile("driverCustomCapabilitesTest", "", new File("./config/"));
+        writeMapToPropertiesFile(properties, tempConfigFile);
+        tempFiles.add(tempConfigFile);
+
+        // this line is important as we initialize the config from the temporary file we created above
+        MultibrowserConfiguration.clearAllInstances();
+        MultibrowserConfiguration.getInstance(tempConfigFile.getPath());
+    }
+
+    @Test
+    public void test()
+    {
+        NeodymiumTestExecutionSummary result = run(DriverCustomCapabilitesTestClass.class);
+        checkPass(result, 4, 0);
+    }
+}
