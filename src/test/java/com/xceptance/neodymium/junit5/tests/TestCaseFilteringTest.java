297f19e7f2744f6ed4a89d58ab58477fdcde45d8
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2026-01-20 13:33:16 +0100

    [#400] revert undesired changes

--- src/test/java/com/xceptance/neodymium/junit5/tests/TestCaseFilteringTest.java
+++ src/test/java/com/xceptance/neodymium/junit5/tests/TestCaseFilteringTest.java
@@ -34,6 +34,8 @@ public class TestCaseFilteringTest extends AbstractNeodymiumTest
     @Test
     public void testTestCaseFiltering()
     {
+        // the test from RandomBrowserChild should run 2 times, as the corresponding annotations should be inherited
+        // from the RandomBrowserParent class
         NeodymiumTestExecutionSummary summary = run(TestCaseFiltering.class);
         checkPass(summary, 2, 3);
     }
