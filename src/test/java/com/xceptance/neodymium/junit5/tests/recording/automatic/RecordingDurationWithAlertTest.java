287a5e589c46ca74bf1c653ead720ba936aacec8
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2026-01-21 19:15:08 +0100

    add retry for RecordingDurationWithAlertTest

--- src/test/java/com/xceptance/neodymium/junit5/tests/recording/automatic/RecordingDurationWithAlertTest.java
+++ src/test/java/com/xceptance/neodymium/junit5/tests/recording/automatic/RecordingDurationWithAlertTest.java
@@ -2,6 +2,8 @@ package com.xceptance.neodymium.junit5.tests.recording.automatic;
 
 import com.xceptance.neodymium.common.recording.FilmTestExecution;
 import com.xceptance.neodymium.common.recording.config.RecordingConfigurations;
+import com.xceptance.neodymium.common.retry.Retry;
+import com.xceptance.neodymium.junit5.NeodymiumTest;
 import com.xceptance.neodymium.junit5.testclasses.recording.CustomRecordingWithAlertTest;
 import com.xceptance.neodymium.junit5.tests.AbstractNeodymiumTest;
 import org.aeonbits.owner.ConfigFactory;
@@ -17,89 +19,90 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RecordingDurationWithAlertTest extends AbstractNeodymiumTest
-{
-    private File recordingFile;
+@Retry(maxNumberOfRetries = 2, exceptions = { "Invalid data found when processing input" })
+public class RecordingDurationWithAlertTest extends AbstractNeodymiumTest {
+	private File recordingFile;
 
-    public double runTest(boolean isGif, String oneImagePerMilliseconds) throws IOException, InterruptedException
-    {
-        CustomRecordingWithAlertTest.isGif = isGif;
-        String format = isGif ? "gif" : "video";
+	public double runTest(boolean isGif, String oneImagePerMilliseconds) throws IOException, InterruptedException {
+		CustomRecordingWithAlertTest.isGif = isGif;
+		String format = isGif ? "gif" : "video";
 
-        FilmTestExecution.clearThreadContexts();
-        Map<String, String> properties1 = new HashMap<>();
-        properties1.put(format + ".filmAutomatically", "false");
-        properties1.put(format + ".enableFilming", "true");
-        properties1.put(format + ".deleteRecordingsAfterAddingToAllureReport", "false");
-        properties1.put(format + ".oneImagePerMilliseconds", oneImagePerMilliseconds);
+		FilmTestExecution.clearThreadContexts();
+		Map<String, String> properties1 = new HashMap<>();
+		properties1.put(format + ".filmAutomatically", "false");
+		properties1.put(format + ".enableFilming", "true");
+		properties1.put(format + ".deleteRecordingsAfterAddingToAllureReport", "false");
+		properties1.put(format + ".oneImagePerMilliseconds", oneImagePerMilliseconds);
 
-        final String fileLocation = "config/temp-" + format + "-" + oneImagePerMilliseconds + ".properties";
-        File tempConfigFile1 = new File("./" + fileLocation);
+		final String fileLocation = "config/temp-" + format + "-" + oneImagePerMilliseconds + ".properties";
+		File tempConfigFile1 = new File("./" + fileLocation);
 
-        writeMapToPropertiesFile(properties1, tempConfigFile1);
-        ConfigFactory.setProperty(FilmTestExecution.TEMPORARY_CONFIG_FILE_PROPERTY_NAME, "file:" + fileLocation);
-        tempFiles.add(tempConfigFile1);
+		writeMapToPropertiesFile(properties1, tempConfigFile1);
+		ConfigFactory.setProperty(FilmTestExecution.TEMPORARY_CONFIG_FILE_PROPERTY_NAME, "file:" + fileLocation);
+		tempFiles.add(tempConfigFile1);
 
-        run(CustomRecordingWithAlertTest.class);
+		run(CustomRecordingWithAlertTest.class);
 
-        RecordingConfigurations config = isGif ? FilmTestExecution.getContextGif() : FilmTestExecution.getContextVideo();
-        recordingFile = new File(config.tempFolderToStoreRecording() + CustomRecordingWithAlertTest.uuid + "." + config.format());
+		RecordingConfigurations config = isGif ? FilmTestExecution.getContextGif()
+				: FilmTestExecution.getContextVideo();
+		recordingFile = new File(
+				config.tempFolderToStoreRecording() + CustomRecordingWithAlertTest.uuid + "." + config.format());
 
-        for (int i = 0; i < 3 && !recordingFile.exists(); i++)
-        {
-            Thread.sleep(1000);
-        }
+		for (int i = 0; i < 3 && !recordingFile.exists(); i++) {
+			Thread.sleep(1000);
+		}
 
-        Assert.assertTrue("the recording file doesn't exist", recordingFile.exists());
-        ProcessBuilder pb = new ProcessBuilder("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", recordingFile.getAbsolutePath());
-        pb.redirectErrorStream(true);
-        Process p = pb.start();
+		Assert.assertTrue("the recording file doesn't exist", recordingFile.exists());
+		ProcessBuilder pb = new ProcessBuilder("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of",
+				"default=noprint_wrappers=1:nokey=1", recordingFile.getAbsolutePath());
+		pb.redirectErrorStream(true);
+		Process p = pb.start();
 
-        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String recordDuration = null;
-        for (String line = r.readLine(); StringUtils.isNotBlank(line); line = r.readLine())
-        {
-            recordDuration = line;
-            continue;
-        }
+		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String recordDuration = null;
+		for (String line = r.readLine(); StringUtils.isNotBlank(line); line = r.readLine()) {
+			recordDuration = line;
+			continue;
+		}
 
-        return Double.parseDouble(recordDuration);
-    }
+		return Double.parseDouble(recordDuration);
+	}
 
-    @Test
-    public void testVideoRecording() throws IOException, InterruptedException
-    {
-        double run100 = runTest(false, "100");
-        double run1000 = runTest(false, "1000");
+	@NeodymiumTest
+	public void testVideoRecording() throws IOException, InterruptedException {
+		double run100 = runTest(false, "100");
+		double run1000 = runTest(false, "1000");
 
-        Assert.assertEquals("Videos with different oneImagePerMilliseconds value should have approximaty the same length (1/100 = " + run1000
-                            + ", 1/1000 = " + run100 + ")", run1000, run1000, 5.0);
-    }
+		Assert.assertEquals(
+				"Videos with different oneImagePerMilliseconds value should have approximaty the same length (1/100 = "
+						+ run1000 + ", 1/1000 = " + run100 + ")",
+				run1000, run1000, 5.0);
+	}
 
-    @Test
-    public void testGifRecording() throws IOException, InterruptedException
-    {
-        double run100 = runTest(true, "100");
-        double run1000 = runTest(true, "1000");
-        Assert.assertEquals("Gifs with different oneImagePerMilliseconds value should have approximaty the same length (1/100 = " + run100 + ", 1/1000 = "
-                            + run1000 + ")", run100, run1000, 5.0);
-    }
+	@NeodymiumTest
+	public void testGifRecording() throws IOException, InterruptedException {
+		double run100 = runTest(true, "100");
+		double run1000 = runTest(true, "1000");
+		Assert.assertEquals(
+				"Gifs with different oneImagePerMilliseconds value should have approximaty the same length (1/100 = "
+						+ run100 + ", 1/1000 = " + run1000 + ")",
+				run100, run1000, 5.0);
+	}
 
-    @Test
-    public void testMixedRecording() throws IOException, InterruptedException
-    {
-        double runVideo1000 = runTest(false, "1000");
-        double runGif1000 = runTest(true, "1000");
-        Assert.assertEquals("Gifs with different oneImagePerMilliseconds value should have approximaty the same length (video = " + runVideo1000 + ", gif = "
-                            + runGif1000 + ")", runVideo1000, runGif1000, 5.0);
-    }
+	@NeodymiumTest
+	public void testMixedRecording() throws IOException, InterruptedException {
+		double runVideo1000 = runTest(false, "1000");
+		double runGif1000 = runTest(true, "1000");
+		Assert.assertEquals(
+				"Gifs with different oneImagePerMilliseconds value should have approximaty the same length (video = "
+						+ runVideo1000 + ", gif = " + runGif1000 + ")",
+				runVideo1000, runGif1000, 5.0);
+	}
 
-    @AfterEach
-    public void cleanup()
-    {
-        if (recordingFile != null)
-        {
-            recordingFile.delete();
-        }
-    }
+	@AfterEach
+	public void cleanup() {
+		if (recordingFile != null) {
+			recordingFile.delete();
+		}
+	}
 }
