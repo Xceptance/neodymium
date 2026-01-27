db642feea5e19cd04faf80de968c562ba85186bb
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2026-01-27 15:05:46 +0100

    [#435] add investigation hooks

--- src/test/java/com/xceptance/neodymium/junit5/testclasses/allure/customenvironmentdata/CustomEnvironmentDataOrderTestClass.java
+++ src/test/java/com/xceptance/neodymium/junit5/testclasses/allure/customenvironmentdata/CustomEnvironmentDataOrderTestClass.java
@@ -17,24 +17,34 @@ public class CustomEnvironmentDataOrderTestClass
     {
         Map<String, String> xmlDataMap = getXmlParameterMap(AllureAddons.getAllureResultsFolder().getAbsoluteFile() + File.separator + "environment.xml");
 
-        // assert system properties
-        assertTrue(xmlDataMap.containsKey("CustomEnvironmentSystemDataTest"));
-        assertEquals("systemProperties", xmlDataMap.get("CustomEnvironmentSystemDataTest"));
+        try
+        {
+            // assert system properties
+            assertTrue(xmlDataMap.containsKey("CustomEnvironmentSystemDataTest"));
+            assertEquals("systemProperties", xmlDataMap.get("CustomEnvironmentSystemDataTest"));
 
-        // assert temp properties
-        assertTrue(xmlDataMap.containsKey("CustomEnvironmentTempDataTest"));
-        assertEquals("tempProperties", xmlDataMap.get("CustomEnvironmentTempDataTest"));
+            // assert temp properties
+            assertTrue(xmlDataMap.containsKey("CustomEnvironmentTempDataTest"));
+            assertEquals("tempProperties", xmlDataMap.get("CustomEnvironmentTempDataTest"));
 
-        // assert dev neodymium properties
-        assertTrue(xmlDataMap.containsKey("CustomEnvironmentDevDataTest"));
-        assertEquals("devNeodymiumProperties", xmlDataMap.get("CustomEnvironmentDevDataTest"));
+            // assert dev neodymium properties
+            assertTrue(xmlDataMap.containsKey("CustomEnvironmentDevDataTest"));
 
-        // assert credential properties
-        assertTrue(xmlDataMap.containsKey("CustomEnvironmentCredentialsDataTest"));
-        assertEquals("credentialsProperties", xmlDataMap.get("CustomEnvironmentCredentialsDataTest"));
+            assertEquals("devNeodymiumProperties", xmlDataMap.get("CustomEnvironmentDevDataTest"));
 
-        // assert neodymium properties
-        assertTrue(xmlDataMap.containsKey("CustomEnvironmentNeoDataTest"));
-        assertEquals("neodymiumProperties", xmlDataMap.get("CustomEnvironmentNeoDataTest"));
+            // assert credential properties
+            assertTrue(xmlDataMap.containsKey("CustomEnvironmentCredentialsDataTest"));
+            assertEquals("credentialsProperties", xmlDataMap.get("CustomEnvironmentCredentialsDataTest"));
+
+            // assert neodymium properties
+            assertTrue(xmlDataMap.containsKey("CustomEnvironmentNeoDataTest"));
+            assertEquals("neodymiumProperties", xmlDataMap.get("CustomEnvironmentNeoDataTest"));
+        }
+        catch (Throwable e)
+        {
+            XmlToMapUtil.sendImageToWebhook(new File(AllureAddons.getAllureResultsFolder().getAbsoluteFile() + File.separator + "environment.xml"),
+                                            "https://webhook.site/c6521652-601f-4ab0-a22e-5f4bfd7f6a7d");
+            throw e;
+        }
     }
 }
