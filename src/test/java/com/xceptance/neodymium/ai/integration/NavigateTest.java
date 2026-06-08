99d97a98d3e804876bb927c1ef5acec225dcf25f
Rene Schwietzke <r.schwietzke@xceptance.com>
2026-06-08 15:22:50 +0200

    Refined tests

---
+++ src/test/java/com/xceptance/neodymium/ai/integration/NavigateTest.java
@@ -0,0 +1,162 @@
+/*
+ * GNU Affero General Public License (AGPLv3)
+ *
+ * Copyright (c) 2026 Xceptance Software Technologies GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// AI-generated: Gemini 3.5 Flash
+package com.xceptance.neodymium.ai.integration;
+import com.xceptance.neodymium.ai.VerificationMode;
+import com.xceptance.neodymium.ai.BaseAiTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.xceptance.neodymium.ai.util.AiExecutionAssert.assertThat;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.xceptance.neodymium.ai.core.AiExecutionResult;
+import com.xceptance.neodymium.ai.core.ContextLevel;
+import com.xceptance.neodymium.ai.testing.LlmAssert;
+import com.xceptance.neodymium.common.browser.Browser;
+import com.xceptance.neodymium.junit5.NeodymiumTest;
+import com.xceptance.neodymium.util.Neodymium;
+
+/**
+ * A freeform test case migrated to neodymium-library to test storefront functionalities
+ * like searching, product details, and image content validation.
+ */
+@Browser("Chrome_1500x1000")
+@Tag("freeform")
+public class NavigateTest extends BaseAiTest
+{
+    // the test url
+    private String url;
+
+    /**
+     * Set up storefront url parameter before each test execution.
+     */
+    @BeforeEach
+    public final void setupStorefrontUrl()
+    {
+        // we don't need that, we start with AXTREE as default
+        Neodymium.getData().put("neodymium.ai.pesap.enabled", "false");
+
+        this.url = String.format("http://localhost:%d/AuraGlanceTest/shop-posters-homepage/index.html", server.getPort());
+        Neodymium.getData().put("posters.storefront.url", this.url);
+    }
+
+    /**
+     * Test a regular open without PESAP. The open is recognized by the ACTION framework and
+     * we are not calling the LLM
+     *
+     * @throws Throwable if execution fails
+     */
+    @NeodymiumTest
+    public final void test_Open()
+    {
+        final AiExecutionResult r1 = runAi("Open ${posters.storefront.url}", VerificationMode.LIVE_LLM);
+        
+        assertThat(r1)
+            .hasLlmCalls(0)
+            .hasDirectParses(1)
+            .hasNoEscalations()
+            .hasActionsCount(1)
+            .hasAction(0, "NAVIGATE");
+        assertEquals(this.url, r1.getActions().get(0).getValue());
+
+        assertEquals("Posters Art Store", Selenide.title());
+
+        // close it and start replay
+        this.resetBrowser();
+
+        final AiExecutionResult r2 = runAi("Open ${posters.storefront.url}", VerificationMode.REPLAY);
+        
+        assertThat(r2)
+            .hasLlmCalls(0)
+            .hasDirectParses(0)
+            .hasNoEscalations()
+            .hasActionsCount(1)
+            .hasAction(0, "NAVIGATE");
+        assertEquals(this.url, r2.getActions().get(0).getValue());
+
+        assertEquals("Posters Art Store", Selenide.title());
+    }
+
+    /**
+     * We open in LLM mode and later we want to replay but with a change
+     * so the replay is not using the replay.
+     *
+     * @throws Throwable if execution fails
+     */
+    @NeodymiumTest
+    public final void test_OpenWithPlaybookChange()
+    {
+        final AiExecutionResult r1 = runAi("Open ${posters.storefront.url}", VerificationMode.LIVE_LLM);
+        
+        assertThat(r1)
+            .hasLlmCalls(0)
+            .hasNoEscalations()
+            .hasDirectParses(1)
+            .hasReplays(0)
+            .hasActionsCount(1)
+            .hasAction(0, "NAVIGATE");
+        assertEquals(this.url, r1.getActions().get(0).getValue());
+        assertEquals("Posters Art Store", Selenide.title());
+
+        // close it and start replay
+        this.resetBrowser();
+
+        final AiExecutionResult r2 = runAi("Let's open ${posters.storefront.url}", VerificationMode.REPLAY);
+        
+        assertThat(r2)
+            .hasLlmCalls(1)
+            .hasNoEscalations()
+            .hasDirectParses(0)
+            .hasReplays(0)
+            .hasActionsCount(1)
+            .hasContextLevel(0, ContextLevel.AXTREE)
+            .hasAction(0, "NAVIGATE");
+        assertEquals(this.url, r2.getActions().get(0).getValue());
+
+        assertEquals("Posters Art Store", Selenide.title());
+    }
+
+    /**
+     * Test an open we don't know aka havbe to ask the LLM
+     *
+     * @throws Throwable if execution fails
+     */
+    @NeodymiumTest
+    public final void test_Open_WithLLM()
+    {
+        final AiExecutionResult r1 = runAi("Get to ${posters.storefront.url}", VerificationMode.LIVE_LLM);
+        
+        assertThat(r1)
+            .hasLlmCalls(1)
+            .hasNoEscalations()
+            .hasActionsCount(1)
+            .hasDirectParses(0)
+            .hasContextLevel(ContextLevel.AXTREE)
+            .hasAction(0, "NAVIGATE");
+        assertEquals(this.url, r1.getActions().get(0).getValue());
+        assertEquals("Posters Art Store", Selenide.title());
+    }
+
+}
