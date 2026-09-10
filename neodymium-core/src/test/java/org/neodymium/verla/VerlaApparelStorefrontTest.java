dc6e3f0947ccb5c31c50d185f14256bf3de112f5
Rene Schwietzke <r.schwietzke@xceptance.com>
2026-09-10 14:28:09 +0200

    refactor(verla): move storefront implementation tests to org.neodymium.verla package

--- neodymium-core/src/test/java/org/neodymium/verla/VerlaApparelStorefrontTest.java
+++ neodymium-core/src/test/java/org/neodymium/verla/VerlaApparelStorefrontTest.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package org.neodymium.ai.integration.verla;
+package org.neodymium.verla;
 
 import java.io.IOException;
 import java.net.CookieManager;
